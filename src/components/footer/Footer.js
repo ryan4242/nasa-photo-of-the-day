e4fb675f978995a4e83b81ace4907e744230acc7
Ryan McInnis <ryan.mcinnis42@gmail.com>
2020-05-28 10:28:29 -0700

    installed styled components and reafactored footer compmmonent

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './footer.css';
+import styled from 'styled-components';
+
+const FooterStyle = styled.footer`
+  background: #2a2d47;
+  color: #ffffff;
+  height: 10vh;
+  box-shadow: 0px 1px 10px 1px rgb(255, 255, 255, 0.7);
+  display: flex;
+  flex-flow: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Footer = props => {
   return (
-    <footer>
+    <FooterStyle>
       <p>&copy; Ryan McInnis</p>
-  <p>Photo rights to {props.owner}</p>
-    </footer>
+      <p>Photo rights to {props.owner}</p>
+    </FooterStyle>
   )
 }
 
